3d45949da80755f779c0222ff73cf4cc9e093edd
Felix Mathiasson <felix.mathiasson99@gmail.com>
2025-04-07 12:52:19 +0200

    added second test to process multiple characters of I

--- src/com/mathiasson/RomanTranslatorTests.java
+++ src/com/mathiasson/RomanTranslatorTests.java
@@ -6,14 +6,22 @@ import org.junit.jupiter.api.Test;
 class RomanTranslatorTests {
 
 	@Test
-	void testRomanOne() 
+	void testRomanOne() // tests I
 	{
 		RomanTranslator rt = new RomanTranslator();
 		int expected = 1;
 		int result = rt.translateRomanToDecimal("I");
 		assertEquals(expected, result);
 	}
-
+	
+	@Test
+	void testRomanTwo() //tests II
+	{
+		RomanTranslator rt = new RomanTranslator();
+		int expected = 2;
+		int result = rt.translateRomanToDecimal("II");
+		assertEquals(expected, result);
+	}
 	
 
 }
