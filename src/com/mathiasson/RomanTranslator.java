63906c5ebc39c2f6589fce044fcb40d165405a94
Felix Mathiasson <felix.mathiasson99@gmail.com>
2025-04-07 12:38:21 +0200

    first test created

--- src/com/mathiasson/RomanTranslator.java
+++ src/com/mathiasson/RomanTranslator.java
@@ -1,5 +1,12 @@
 package com.mathiasson;
 
 public class RomanTranslator {
-
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public int translateRomanToDecimal(String string) {
+		return 0;
+	} 
 }
--- src/com/mathiasson/RomanTranslatorTests.java
+++ src/com/mathiasson/RomanTranslatorTests.java
@@ -1,14 +1,19 @@
 package com.mathiasson;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class RomanTranslatorTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testRomanOne() 
+	{
+		RomanTranslator rt = new RomanTranslator();
+		int expected = 1;
+		int result = rt.translateRomanToDecimal("I");
+		assertEquals(expected, result);
 	}
+
+	
 
 }
